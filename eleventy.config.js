074e86407ecbee92c43852ed8df135f982f4de15
Patrick Parschan <patrick.parschan@web.de>
2024-11-28 09:06:10 +0100

    update eleventy-config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,83 +1,33 @@
-const navigation = require('@11ty/eleventy-navigation')
-const dates = require('./utilities/filters/dates')
-const helpers = require('./utilities/filters/helpers')
-const path = require('path')
+const navigation = require('@11ty/eleventy-navigation');
+const dates = require('./utilities/filters/dates');
+const helpers = require('./utilities/filters/helpers');
+const htmlminify = require('./utilities/transforms/htmlminify');
 
-module.exports = config => {
-  // navigation plugin
+module.exports = function (config) {
+  // Plugins
   config.addPlugin(navigation);
 
-  // Human readable date for posts
+  // Filters
   config.addFilter("dateDisplay", dates.friendly);
-
-  // Timestamp for datetime element
   config.addFilter("timestamp", dates.timestamp);
-
-  // Remove whitespace from a string
   config.addNunjucksFilter("spaceless", helpers.spaceless);
+  config.addNunjucksFilter("shuffle", helpers.shuffle);
 
-  // Minify our HTML
-  config.addTransform(
-    "htmlminify",
-    require("./utilities/transforms/htmlminify")
-  );
+  // Transforms
+  config.addTransform("htmlminify", htmlminify);
 
-  // // Collections
-  // config.addCollection("blog", (collection) => {
-  //   const blogs = collection.getFilteredByTag("blog");
+  // Ignored Files
+  config.ignores.add("_drafts/**");
 
-  //   for (let i = 0; i < blogs.length; i++) {
-  //     const previous_post = blogs[i - 1];
-  //     const next_post = blogs[i + 1];
+  // Layout Aliases
+  config.addLayoutAlias("base", "_layouts/base.njk");
+  config.addLayoutAlias("home", "_layouts/home.njk");
+  config.addLayoutAlias("page", "_layouts/page.njk");
+  config.addLayoutAlias("organizers", "_layouts/organizers.njk");
+  config.addLayoutAlias("sponsorship", "_layouts/sponsorship.njk");
+  config.addLayoutAlias("contact", "_layouts/contact.njk");
 
-  //     blogs[i].data["previous_post"] = previous_post;
-  //     blogs[i].data["next_post"] = next_post;
-  //   }
-
-  //   return blogs.reverse();
-  // });
-
-  // // Categories collection
-  // config.addCollection("categories", (collection) => {
-  //   const list = new Set();
-
-  //   collection.getAll().forEach((item) => {
-  //     if (!item.data.tags) return;
-
-  //     item.data.tags
-  //       .filter((category) => !["blog", "all"].includes(category))
-  //       .forEach((category) => list.add(category));
-  //   });
-
-  //   return Array.from(list).sort();
-  // });
-
-  // Shuffle filter
-
-  // Random Filter: With the help from google search engine
-  config.addNunjucksFilter("shuffle", function (array) {
-    return helpers.shuffle(array);
-  });
-
-  module.exports = function (eleventyConfig) {
-    eleventyConfig.ignores.add("_drafts/**");
-    eleventyConfig.ignores.add("layouts/blog.njk");
-    eleventyConfig.ignores.add("layouts/post.njk");
-    eleventyConfig.ignores.add("layouts/category.njk");
-  };
-
-  // Layout aliases
-  config.addLayoutAlias("base", "layouts/base.njk");
-  config.addLayoutAlias("home", "layouts/home.njk");
-  config.addLayoutAlias("page", "layouts/page.njk");
-  // config.addLayoutAlias("blog", "layouts/blog.njk");
-  // config.addLayoutAlias("post", "layouts/post.njk");
-  config.addLayoutAlias("organizers", "layouts/organizers.njk");
-  config.addLayoutAlias("sponsorship", "layouts/sponsorship.njk");
-  config.addLayoutAlias("contact", "layouts/contact.njk");
-  // config.addLayoutAlias("category", "layouts/category.njk");
-
-  // Include our static assets
+  // Passthrough Copy
   config.addPassthroughCopy("css");
   config.addPassthroughCopy("js");
   config.addPassthroughCopy("images");
@@ -85,13 +35,17 @@ module.exports = config => {
   config.addPassthroughCopy("favicon.png");
   config.addPassthroughCopy("favicon.svg");
 
+  // Return Configuration
   return {
     markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     dir: {
       input: "site",
       output: "public",
       includes: "includes",
+      layouts: "_layouts",
       data: "globals",
     },
   };
-}
+};
